ce48f90d2f0802a00d82155099f1585ee1a88ceb
ilya <ilya@lagoshny.ru>
2019-10-21 20:53:09 +0300

    Added configuration for ngx-hal-client, ngx-validation-messages, and routing modules

--- main/src/app/app.module.ts
+++ main/src/app/app.module.ts
@@ -1,13 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxHalClientModule } from '@lagoshny/ngx-hal-client';
+import { NgxValidationMessagesModule } from '@lagoshny/ngx-validation-messages';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { AppComponent } from './app.component';
+import { ExternalConfigurationService } from './app.config';
+import { AppRoutingModule } from './app.routing.module';
+import { ValidationMessagesConfig } from './core/validation/validation-messages.config';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        NgxHalClientModule.forRoot(),
+        NgxValidationMessagesModule.forRoot({
+            messages: ValidationMessagesConfig.getMessages(),
+            validationMessagesStyle: {
+                blockClassNames: 'error_block'
+            }
+        }),
+        LoggerModule.forRoot({
+            level: NgxLoggerLevel.DEBUG
+        }),
+        HomeModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent
+    ],
+    providers: [
+        {provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService}
     ],
     bootstrap: [AppComponent]
 })
