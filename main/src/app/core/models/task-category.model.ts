1301a498ef8f28422b58eef4e35683ff6b2fb807
ilya <ilya@lagoshny.ru>
2019-10-21 20:46:58 +0300

    Added models

---
+++ main/src/app/core/models/task-category.model.ts
@@ -0,0 +1,45 @@
+import { Resource } from '@lagoshny/ngx-hal-client';
+import { User } from './user.model';
+
+export class TaskCategory extends Resource {
+
+    /**
+     * Category id.
+     */
+    public id: string;
+
+    /**
+     * Category owner.
+     */
+    public user: User;
+
+    /**
+     * Name of the category.
+     */
+    public name: string;
+
+    /**
+     * Category description.
+     */
+    public description: string;
+
+    /**
+     * Prefix which forms for task number.
+     */
+    public prefix: string;
+
+    /**
+     * Category icon.
+     */
+    public icon: string;
+
+    /**
+     * Category color.
+     */
+    public color: string;
+
+    public constructor() {
+        super();
+    }
+
+}
