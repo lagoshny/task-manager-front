e1435936b0431907e0f891620822179e0f04deb7
ilya <ilya@lagoshny.ru>
2019-10-21 20:45:03 +0300

    Added core module with mock auth service

---
+++ main/src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { AuthService } from './services/auth.service';
+
+
+@NgModule({
+    providers: [
+        AuthService
+    ]
+})
+export class CoreModule {
+}
