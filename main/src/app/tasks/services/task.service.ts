fe2553a2be6811813080bc8bebad5b8e5acf7cd0
ilya <ilya@lagoshny.ru>
2019-10-22 21:15:54 +0300

    Added task-list component

--- main/src/app/tasks/services/task.service.ts
+++ main/src/app/tasks/services/task.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, Injector } from '@angular/core';
-import { RestService } from '@lagoshny/ngx-hal-client';
+import { ResourcePage, RestService } from '@lagoshny/ngx-hal-client';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { ServerApi } from '../../app.config';
+import { TaskStatus } from '../../core/models/constants/task-status.items';
 import { Task } from '../../core/models/task.model';
+import { AuthService } from '../../core/services/auth.service';
 
 @Injectable()
 export class TaskService extends RestService<Task> {
 
-    constructor(injector: Injector) {
+    constructor(injector: Injector,
+                private authService: AuthService) {
         super(Task, ServerApi.TASKS.resource, injector);
     }
 
@@ -17,6 +21,27 @@ export class TaskService extends RestService<Task> {
 
     public updateTask(task: Task): Observable<Task | Observable<never>> {
         return this.patch(task);
+    }
+
+    public getAllUserTasks(taskPageSize: number): Observable<ResourcePage<Task>> {
+        const author = this.authService.getUser();
+        return this.searchPage(ServerApi.TASKS.allByAuthor.query, {
+            size: taskPageSize,
+            params: [
+                {
+                    key: ServerApi.TASKS.allByAuthor.authorParam,
+                    value: author.id
+                },
+                ServerApi.TASKS.projections.taskProjection
+            ]
+        })
+            .pipe(
+                tap((tasks: ResourcePage<Task>) => {
+                    tasks.resources.forEach((task: Task) => {
+                        task.status = TaskStatus.getByCode(task.status).name;
+                    });
+                })
+            );
     }
 
 }
--- main/src/app/tasks/tasks.module.ts
+++ main/src/app/tasks/tasks.module.ts
@@ -4,6 +4,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgxValidationMessagesModule } from '@lagoshny/ngx-validation-messages';
 import { CoreModule } from '../core/core.module';
 import { QuickTaskCreateComponent } from './components/quick-task-create/quick-task-create.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
+import { TimeIconComponent } from './components/task/time-icon/time-icon.component';
 import { TaskService } from './services/task.service';
 
 @NgModule({
@@ -14,12 +17,15 @@ import { TaskService } from './services/task.service';
         NgxValidationMessagesModule
     ],
     declarations: [
-        QuickTaskCreateComponent
+        QuickTaskCreateComponent,
+        TaskComponent,
+        TimeIconComponent,
+        TaskListComponent
     ],
     providers: [
         TaskService
     ],
-    exports: [QuickTaskCreateComponent]
+    exports: [TaskListComponent]
 })
 export class TasksModule {
 }
