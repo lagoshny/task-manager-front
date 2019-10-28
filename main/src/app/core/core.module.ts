a9b6db5c9d296c6a0a35026ec31e82b923e9e6e4
ilya <ilya@lagoshny.ru>
2019-10-28 22:03:23 +0300

    Added common-page component to represent standard ui form

--- main/src/app/core/core.module.ts
+++ main/src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonPageComponent } from './components/common-page/common-page.component';
 import { AuthService } from './services/auth.service';
 
 
 @NgModule({
+    declarations: [
+        CommonPageComponent
+    ],
     providers: [
         AuthService
+    ],
+    exports: [
+      CommonPageComponent
     ]
 })
 export class CoreModule {
