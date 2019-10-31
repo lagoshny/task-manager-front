0ad62b055ed2d4ca6f34e4ee8fb81e675bf88582
ilya <ilya@lagoshny.ru>
2019-10-31 20:45:48 +0300

    Added common select class to hold common logic

--- main/src/app/core/models/constants/task-priority.items.ts
+++ main/src/app/core/models/constants/task-priority.items.ts
@@ -1,12 +1,21 @@
+import { CommonSelectItem } from './common-select-item.constant';
 
-export class TaskPriority {
+export class TaskPriority extends CommonSelectItem<TaskPriority> {
 
     public static HIGH = new TaskPriority('High', 'HIGH', '', 'red', 'fa-exclamation-circle');
     public static MIDDLE = new TaskPriority('Middle', 'MIDDLE', '', 'orange', 'fa-bolt');
     public static LOWER = new TaskPriority('Low', 'LOW', '', 'green', 'fa-level-down');
 
-    constructor(public name?: string, public code?: string,
-                public className?: string, public color?: string, public icon?: string) {
+    public static getAll(): Array<TaskPriority> {
+        return TaskPriority.getAllByType<TaskPriority>(TaskPriority);
+    }
+
+    public static getByCode(code: any): TaskPriority {
+        return TaskPriority.getTypeByCode(TaskPriority, code);
+    }
+
+    public static getByName(name: any): TaskPriority {
+        return TaskPriority.getTypeByName(TaskPriority, name);
     }
 
 }
