eba00496ea49a2708aa72ad28dd2173622fa0011
ilya <ilya@lagoshny.ru>
2019-11-05 21:13:54 +0300

    Added amount characters pipe

---
+++ main/src/app/core/pipes/amount-characters.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { StringUtils } from '../utils/string.utils';
+
+/**
+ * Pipe to control max length of the string.
+ * If string exceeds max characters passed to pipe then exceeded part of the string replace with ellipsis.
+ */
+@Pipe({
+    name: 'amountCharacters'
+})
+export class AmountCharactersPipe implements PipeTransform {
+
+    public transform(str: string, maxCharacters: number): string {
+        if (str.length === maxCharacters) {
+            return str;
+        }
+        const maxCharactersWithEllipsis = maxCharacters - 3;
+        if (str.length > maxCharactersWithEllipsis) {
+            const separatedString = str.slice(0, maxCharactersWithEllipsis).split(StringUtils.SPACE);
+            separatedString.slice(separatedString.length - 1, 1);
+            return separatedString.join(StringUtils.SPACE).trim() + StringUtils.ELLIPSIS;
+        }
+
+        return str;
+    }
+
+}
