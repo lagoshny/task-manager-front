bc024297a080d1c3e1134f40a3f1ff4a5218f8bd
ilya <ilya@lagoshny.ru>
2019-11-05 21:15:59 +0300

    Added amount pipe to core module with export to use in another modules

--- main/src/app/core/core.module.ts
+++ main/src/app/core/core.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonPageComponent } from './components/common-page/common-page.component';
+import { AmountCharactersPipe } from './pipes/amount-characters.pipe';
 import { AuthService } from './services/auth.service';
 
 
 @NgModule({
     declarations: [
-        CommonPageComponent
+        CommonPageComponent,
+        AmountCharactersPipe
     ],
     providers: [
         AuthService
     ],
     exports: [
-      CommonPageComponent
+        CommonPageComponent,
+        AmountCharactersPipe
     ]
 })
 export class CoreModule {
