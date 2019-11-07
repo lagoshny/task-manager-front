3beb3d0ab87a910e6471287000f10a3ab7f46a5b
ilya <ilya@lagoshny.ru>
2019-11-07 22:16:27 +0300

    Added dialog to choose category icon from fontawesome icons

--- main/src/app/core/core.module.ts
+++ main/src/app/core/core.module.ts
@@ -1,20 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { CommonPageComponent } from './components/common-page/common-page.component';
+import { FontIconListDialogComponent } from './components/font-icon-list-dialog/font-icon-list-dialog.component';
 import { AmountCharactersPipe } from './pipes/amount-characters.pipe';
 import { AuthService } from './services/auth.service';
+import { FontIconService } from './services/font-icon.service';
 
 
 @NgModule({
+    imports: [
+        CommonModule,
+        MatDialogModule
+    ],
     declarations: [
         CommonPageComponent,
+        FontIconListDialogComponent,
         AmountCharactersPipe
     ],
     providers: [
-        AuthService
+        AuthService,
+        FontIconService
     ],
     exports: [
         CommonPageComponent,
         AmountCharactersPipe
+    ],
+    entryComponents: [
+        FontIconListDialogComponent
     ]
 })
 export class CoreModule {
