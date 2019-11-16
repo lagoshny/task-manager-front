d8ee1c41d92b7559d84cfdf64bd31b08c9d086fb
ilya <ilya@lagoshny.ru>
2019-11-16 15:25:41 +0300

    Added error interceptor that intercept server error requests and show error on interface using ServerNotificationComponent

--- main/src/app/core/core.module.ts
+++ main/src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CommonPageComponent } from './components/common-page/common-page.component';
 import { FontIconListDialogComponent } from './components/font-icon-list-dialog/font-icon-list-dialog.component';
+import { ServerNotificationComponent } from './components/server-notification/server-notification.component';
 import { SimpleDialogComponent } from './components/simple-dialog/simple-dialog.component';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { AmountCharactersPipe } from './pipes/amount-characters.pipe';
 import { AuthService } from './services/auth.service';
 import { FontIconService } from './services/font-icon.service';
@@ -14,17 +18,20 @@ import { FontIconService } from './services/font-icon.service';
     imports: [
         CommonModule,
         MatDialogModule,
-        MatButtonModule
+        MatButtonModule,
+        MatSnackBarModule
     ],
     declarations: [
         CommonPageComponent,
         FontIconListDialogComponent,
         SimpleDialogComponent,
+        ServerNotificationComponent,
         AmountCharactersPipe
     ],
     providers: [
         AuthService,
-        FontIconService
+        FontIconService,
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
     ],
     exports: [
         CommonPageComponent,
@@ -32,7 +39,8 @@ import { FontIconService } from './services/font-icon.service';
     ],
     entryComponents: [
         FontIconListDialogComponent,
-        SimpleDialogComponent
+        SimpleDialogComponent,
+        ServerNotificationComponent
     ]
 })
 export class CoreModule {
