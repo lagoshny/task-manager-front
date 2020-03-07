7b24a904ab3742977744ec95263aae6a4fd7ff9b
ilya <ilya@lagoshny.ru>
2020-03-07 20:04:58 +0300

    Added event to invoke filter by selected categories

--- main/src/app/core/services/task-category.service.ts
+++ main/src/app/core/services/task-category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { TaskCategory } from '../models/task-category.model';
 
 /**
  * Service allows manage dependencies between tasks and categories.
@@ -7,9 +8,20 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class TaskCategoryService {
 
+    public categoriesToFilter = new Subject<Array<TaskCategory>>();
+
     public tasks = new Subject<void>();
 
     public categories = new Subject<void>();
+
+    /**
+     * Generates a filtering event for the selected category list.
+     *
+     * @param categories list of categories for filtering
+     */
+    public updateCategoriesToFilter(categories: Array<TaskCategory>): void {
+        this.categoriesToFilter.next(categories);
+    }
 
     /**
      * Generates an event that needs to update the task list.
