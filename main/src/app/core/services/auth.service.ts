0e068f1ca3a408bbbe9f293d18b00e4f8a4e59b9
ilya <ilya@lagoshny.ru>
2020-03-16 21:22:27 +0300

    Replace fake impl AuthService to real User's data

--- main/src/app/core/services/auth.service.ts
+++ main/src/app/core/services/auth.service.ts
@@ -1,19 +1,59 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 import { User } from '../models/user.model';
+import { AuthStorageUtils } from '../utils/auth-storage.utils';
+import { UserStorageUtils } from '../utils/user-storage.utils';
 
 @Injectable()
 export class AuthService {
 
+    /**
+     * Get user's data from local storage.
+     */
     public getUser(): User {
-        let fakeUser = new User();
-        fakeUser.id = 1;
-        fakeUser._links = {
-            self: {
-                href: 'http://localhost:8080/api/v1/users/1'
-            }
-        };
+        return UserStorageUtils.getUser();
+    }
 
-        return fakeUser;
+    /**
+     * Set user's data to local storage.
+     */
+    public setAuthUser(user: User) {
+        UserStorageUtils.saveUser(user);
+    }
+
+    /**
+     * Check that current user is authenticated.
+     */
+    public isAuthenticated(): boolean {
+        return !_.isEmpty(AuthStorageUtils.getCredentials());
+    }
+
+    /**
+     * Save auth credentials to local storage.
+     *
+     * @param credentials auth credentials to save
+     */
+    public setCredentials(credentials: string): void {
+        AuthStorageUtils.clearCredentials();
+        if (_.isEmpty(credentials)) {
+            throw new Error('Credentials cannot be empty!');
+        }
+        AuthStorageUtils.saveCredentials(credentials);
+    }
+
+    /**
+     * Get auth credentials from local storage.
+     */
+    public getCredentials(): string {
+        return AuthStorageUtils.getCredentials();
+    }
+
+    /**
+     * Clear user's credentials.
+     */
+    public logOut(): void {
+        AuthStorageUtils.clearCredentials();
+        UserStorageUtils.clearUser();
     }
 
 }
