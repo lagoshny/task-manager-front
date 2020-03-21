3728487fff6dec36cfddbce2caa94d2dce5c7731
ilya <ilya@lagoshny.ru>
2020-03-21 16:54:39 +0300

    Added auth guard to redirect to login form when user isn't authenticated

--- main/src/app/app.routing.module.ts
+++ main/src/app/app.routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryFormComponent } from './categories/components/category-form/category-form.component';
+import { AuthGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { TaskFormComponent } from './tasks/components/task-form/task-form.component';
 
 export const routes: Routes = [
     {
         path: '',
+        canActivate: [AuthGuard],
         children: [
             {
                 path: 'home',
