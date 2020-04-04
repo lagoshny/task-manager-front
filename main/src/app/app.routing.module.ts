f33b61c8a1b3734f9bd3996ec676cea025580904
ilya <ilya@lagoshny.ru>
2020-04-04 15:30:47 +0300

    Added user-form to app routing

--- main/src/app/app.routing.module.ts
+++ main/src/app/app.routing.module.ts
@@ -4,6 +4,7 @@ import { CategoryFormComponent } from './categories/components/category-form/cat
 import { AuthGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { TaskFormComponent } from './tasks/components/task-form/task-form.component';
+import { UserFromComponent } from './users/components/user-from/user-from.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,10 @@ export const routes: Routes = [
             {
                 path: 'home',
                 component: HomeComponent
+            },
+            {
+                path: 'users',
+                component: UserFromComponent
             },
             {
                 path: 'categories',
