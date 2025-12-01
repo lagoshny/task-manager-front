192c8b25ebc8444a687f596eb5ad23d5864285d4
Ilya Lagoshny <ilya@lagoshny.ru>
2025-12-01 23:08:58 +0300

    Update to Angular 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { HeaderModule } from './header/header.module';
 
 @Component({
-    selector: 'tm-app',
-    templateUrl: './app.component.html',
-    imports: [
-        RouterOutlet,
-    ]
+  selector: 'tm-app',
+  templateUrl: './app.component.html',
+  imports: [
+    RouterOutlet,
+    HeaderModule,
+  ],
+  standalone: true,
 })
 export class AppComponent {
 
